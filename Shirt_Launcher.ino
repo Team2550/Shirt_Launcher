fec91641f0089d5063fa7ca5b22dabff8eedf6fa
ProgramLaptop <team2550robotics@gmail.com>
2015-10-06 15:53:53 -0700

    Added steering file

--- Shirt_Launcher.ino
+++ Shirt_Launcher.ino
@@ -1,9 +1,13 @@
 const int time_on = 500;
 
 const int outputs[] = {P2_5, P2_4, P1_5, P1_4, P1_3, P1_2, P4_3, P4_0, P3_7, P8_2};
-const int outputs_len = 6; // Change to limit output number
-const int signal = P8_2;
-const int minSignalLength = 1500;
+const int cannon_outputs = 6; // Change to limit output number
+const int launchSignal = P8_2;
+const int steerSignal = P3_7;
+const int potentSignal = P4_0;
+const int steering = ;
+// Switch signal inputs to analog inputs to make room for other outputs
+const int launchSignalLength = 1500;
 
 #define LED RED_LED
 
@@ -15,19 +19,19 @@ long sigLen = 0;
 void setup() {
   // initialize the pins as outputs:
   
-  for (int i = 0; i < outputs_len; i++) {
+  for (int i = 0; i < cannon_outputs; i++) {
     pinMode(outputs[i], OUTPUT);
     digitalWrite(outputs[i], LOW);
   }
   // initialize the pushbutton pin as an input:
   pinMode(LED, OUTPUT);      
-  pinMode(signal, INPUT);
+  pinMode(launchSignal, INPUT);
 }
 
 void loop()
 {
-  sigLen = pulseIn(signal, HIGH);
-  buttonPressed = sigLen > minSignalLength;
+  sigLen = pulseIn(launchSignal, HIGH);
+  buttonPressed = sigLen > launchSignalLength;
 
   if (buttonPressed && !isDown) {
     digitalWrite(LED, HIGH);
@@ -38,7 +42,7 @@ void loop()
     digitalWrite(outputs[nextOutput], LOW); 
     
     nextOutput++;
-    if (nextOutput >= outputs_len) {
+    if (nextOutput >= cannon_outputs) {
       nextOutput = 0;
     } 
     
