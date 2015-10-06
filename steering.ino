ce4c98bc8c5e8335aa665b0cfb8bb9925ecd79c9
ProgramLaptop <team2550robotics@gmail.com>
2015-10-06 16:46:58 -0700

    Finished theoretical code. Needs to be tested.

--- steering.ino
+++ steering.ino
@@ -1,10 +1,17 @@
+//ENERGIA
 const int steerSignal = P6_1;
 const int potentiometer = P6_2;
 const int steeringMotor = P8_2;
 
-const int centerVal;
+const long joystickMin = 500;
+const long joystickMax = 2500;
+const int potentMin = 100;
+const int potentMax = 923;
+
+const int centerVal = 512;
 
 long steerLength = 0;
+long steeringValue;
 int potentVal;
 int targetVal;
 
@@ -12,11 +19,22 @@ int targetVal;
 void steering_setup() {
   pinMode(steerSignal, INPUT);
   //pinMode(potentSignal, INPUT);
-  pinMode(steeringMotor, OUTPUT);
+  //pinMode(steeringMotor, OUTPUT);
 }
 
 void steering_loop() {
-  steering = pulseIn(launchSignal, HIGH);
+  steerLength = pulseIn(launchSignal, HIGH);
   potentVal = analogRead(potentiometer);
   
+  steeringValue = ((steerLength-joystickMin)/(joystickMax-joystickMin));
+  steeringValue *= 255;
+  potentVal = map(potentVal, 0, 1023, 0, 255);
+  
+  if (potentVal > steeringValue) {
+     analogWrite(steeringMotor, 0);
+  } else if (potentVal < steeringValue) {
+     analogWrite(steeringMotor, 255);
+  } else {
+     analogWrite(steeringMotor, 127);
+  }
 }
